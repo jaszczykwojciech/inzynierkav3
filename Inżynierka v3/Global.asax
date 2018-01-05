3370204d47f9a478e7191af8dc791cd5bc5ccfd2
jaszczykwojciech <jaszczykwojciech@gmail.com>
2018-01-05 12:17:00 +0100

    Dodaj pliki projektów.

---
+++ Inżynierka v3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Inżynierka_v3.MvcApplication" Language="C#" %>
---
+++ Inżynierka v3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Inżynierka_v3.Startup))]
+namespace Inżynierka_v3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
